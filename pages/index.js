c5df2c49e60db0b291a359b58f40bd0f1f28c49b
Riddhima Chauhan <149195451+riddhimachauhan@users.noreply.github.com>
2025-07-07 12:43:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,20 @@
+import HeroSection from "../components/HeroSection";
+import FeaturesSection from "../components/FeaturesSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import HowItWorksSection from "../components/HowItWorksSection";
+import CallToActionSection from "../components/CallToActionSection";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <TestimonialsSection />
+      <HowItWorksSection />
+      <CallToActionSection />
+      <Footer />
+    </>
+  );
+}
 
